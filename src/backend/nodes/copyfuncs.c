6027463d936667157f8c571c4e58324f8a32ecfd
Junseok Yang <jsyang@bitnine.net>
2017-03-09 02:40:10 -0800

    fix: Review the implementation
    * Some bug fixes
    * Code refactoring and cleanup
    
    (cherry picked from commit af054a3a3d85cc89cff2d139cb617c82b9dbadfb)

--- src/backend/nodes/copyfuncs.c
+++ src/backend/nodes/copyfuncs.c
@@ -757,7 +757,7 @@ _copyNestLoop(const NestLoop *from)
 static NestLoopVLE *
 _copyNestLoopVLE(const NestLoopVLE *from)
 {
-	NestLoopVLE   *newnode = makeNode(NestLoopVLE);
+	NestLoopVLE *newnode = makeNode(NestLoopVLE);
 
 	/*
 	 * copy node superclass fields
@@ -767,8 +767,9 @@ _copyNestLoopVLE(const NestLoopVLE *from)
 	/*
 	 * copy remainder of node
 	 */
-	/* FIXME: ugly */
+
 	COPY_NODE_FIELD(nl.nestParams);
+
 	COPY_SCALAR_FIELD(minHops);
 	COPY_SCALAR_FIELD(maxHops);
 
