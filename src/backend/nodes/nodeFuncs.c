82ff49dacfde4be5519426ab70b508f4d2a5e830
Junseok Yang <jsyang@bitnine.net>
2017-08-23 14:47:35 -0700

    fix: Add missing `break` statement
    (cherry picked from commit 2252b5001e51843acb22540fd1f9886908db0930)

--- src/backend/nodes/nodeFuncs.c
+++ src/backend/nodes/nodeFuncs.c
@@ -1576,6 +1576,7 @@ exprLocation(const Node *expr)
 				loc = leftmostLoc(m->location,
 								  exprLocation((Node *) m->keyvals));
 			}
+			break;
 		default:
 			/* for any other node type it's just unknown... */
 			loc = -1;
