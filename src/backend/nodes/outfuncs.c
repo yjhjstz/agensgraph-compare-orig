e8a360aca90dc68a5828f7afcdb84d31d0193ad6
Junseok Yang <jsyang@bitnine.net>
2017-08-31 16:24:16 -0700

    fix: Handle null values properly
    And some minor corrections;
    
    * Refactor code and clean up coding style
    * Remove unnecessary files
    
    (cherry picked from commit 1ff4606b0e83ccb8541590c381d487fc08326534)

--- src/backend/nodes/outfuncs.c
+++ src/backend/nodes/outfuncs.c
@@ -1682,6 +1682,7 @@ _outCypherAccessExpr(StringInfo str, const CypherAccessExpr *node)
 	WRITE_NODE_FIELD(path);
 }
 
+
 /*****************************************************************************
  *
  *	Stuff from relation.h.
