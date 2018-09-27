4f6b234a3042f31b54918d6fd045a3aa211590c4
Hyunsu Choi <hyunsu.choi@bitnet.net>
2018-09-27 17:51:32 +0900

    fix: fix incorrect spacePeak of shortestpath
    (cherry picked from commit 63cf267ab56e80d7303be5cd4571b39a92ec8fa8)

--- src/include/nodes/execnodes.h
+++ src/include/nodes/execnodes.h
@@ -2209,6 +2209,7 @@ typedef struct Hash2SideState
 	PlanState      *spstate;
 	double          totalPaths;
 	long            hops;
+	Size            spacePeak;
 } Hash2SideState;
 
 typedef struct ShortestpathState
